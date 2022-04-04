d1f3bdab07bc8fb6164c0fce6035e54c57ed2bf7
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-04-04 10:56:30 +0200

    migliorato codice login

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/service/api.service';
 
@@ -17,30 +18,50 @@ export class LoginPageComponent implements OnInit {
     password: new FormControl('', [Validators.required])
   });
 
-  constructor(private api: ApiService, private router: Router) { }
+  constructor(private api: ApiService, private router: Router, private snackBar: MatSnackBar) { 
+
+  }
 
   ngOnInit(): void {
 
   }
   
   login(){
-    this.api.checkUserLogin(this.loginForm.value).subscribe({
-      next: res => {
-        const user = res.find((a: any) => {
-          return a.username === this.loginForm.value.username && a.password === this.loginForm.value.password
-        });
+    // this.api.checkUserLogin(this.loginForm.value).subscribe({
+    //   next: res => {
+    //     const user = res.find((a: any) => {
+    //       return a.username === this.loginForm.value.username && a.password === this.loginForm.value.password
+    //     });
+    //     if (user) {
+    //       //alert('Login Succesful');
+    //       this.loginForm.reset();
+    //       this.api.loginState$.next(true);
+    //       this.router.navigate(['private']);
+    //     } else {
+    //       //alert("user not found");
+    //       this.logged = false;
+    //     }
+    //   },
+    //   error: () => {
+    //     alert("Something went wrong")
+    //   }
+    // });
+    this.snackBar.open('Loading...');
+    this.api.login(this.loginForm.value.username, this.loginForm.value.password).subscribe({
+      next: (user) => {
+        this.snackBar.dismiss();
         if (user) {
-          //alert('Login Succesful');
-          this.loginForm.reset();
-          this.api.loginState$.next(true);
           this.router.navigate(['private']);
         } else {
-          //alert("user not found");
-          this.logged = false;
+          this.snackBar.open('Utente non trovato, controlla i dati di accesso.', '', {
+            duration: 2 * 1000,
+          });
         }
       },
-      error: () => {
-        alert("Something went wrong")
+      error: (error) => {
+        this.snackBar.open(error.message, '', {
+          duration: 2 * 1000,
+        });
       }
     });
   }
